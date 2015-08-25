897f882e6a99804c9acf8e2ea6e011f561e532e1
Maximiliano Ruiz <maximiliano.ruiznieto@tallertechnologies.com>
2015-08-25 11:12:15 -0300

    Splash ready

--- app/src/main/java/com/example/maximiliano/aquarius/HomeActivity.java
+++ app/src/main/java/com/example/maximiliano/aquarius/HomeActivity.java
@@ -1,44 +1,25 @@
 package com.example.maximiliano.aquarius;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SplashActivity extends AppCompatActivity {
-
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+public class HomeActivity extends AppCompatActivity {
 
     ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_home);
 
         actionBar = getSupportActionBar();
         actionBar.setIcon(R.mipmap.sat_icon);
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFE8E8E8")));
-
-        Thread splash = new Thread() {
-            public void run() {
-                try {
-                    sleep(3*1000);
-                    Intent intent = new Intent(getBaseContext(),HomeActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        splash.start();
     }
 }
