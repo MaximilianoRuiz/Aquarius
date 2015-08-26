47c21a30fadd83e405084df30fe145feb2253cbc
Maxi <maxi_ruiz140@hotmail.com>
2015-08-26 00:25:07 -0300

    Gallery View Holder ready

--- app/src/main/java/com/example/maximiliano/aquarius/GalleryAdapter.java
+++ app/src/main/java/com/example/maximiliano/aquarius/GalleryAdapter.java
@@ -1,22 +1,17 @@
 package com.example.maximiliano.aquarius;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
-
-import java.util.List;
 
 public class GalleryAdapter extends ArrayAdapter{
 
     Context context;
     String[] list;
 
-    TextView textView;
 
     public GalleryAdapter(Context context, String[] list) {
         super(context, R.layout.gallery_list_adapter, list);
@@ -24,13 +19,22 @@ public class GalleryAdapter extends ArrayAdapter{
         this.list = list;
     }
 
+    public static class GalleryViewHolder{
+        public final TextView textView;
+
+        public GalleryViewHolder(View v) {
+            textView = (TextView) v.findViewById(R.id.textView);
+        }
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row = inflater.inflate(R.layout.gallery_list_adapter, parent, false);
 
-        textView = (TextView) row.findViewById(R.id.textView);
-        textView.setText(list[position]);
+        GalleryViewHolder holder = new GalleryViewHolder(row);
+
+        holder.textView.setText(list[position]);
 
         return row;
     }
